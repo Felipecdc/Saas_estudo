11cb165eb52e6ebde3bbc5a3b64f10e016489f53
Felipe Castro <felipecdc09@gmail.com>
2024-02-24 11:29:40 -0300

    vercel

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
-import express, { Request, Response, NextFunction } from 'express'
-import 'express-async-errors'
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
-import { router } from './routes'
+import { router } from './routes';
+import pg, { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  (req as any).pool = pool;
+  next();
+})
 
 app.use(router);
 
